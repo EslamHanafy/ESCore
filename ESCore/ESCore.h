9cb9ed84491d140ee8b2d9c76fc7e17b98c00f5d
EslamHanafy <eslamhanafy1998@gmail.com>
2019-12-29 11:15:29 +0200

    Initial Commit

---
+++ ESCore/ESCore.h
@@ -0,0 +1,19 @@
+//
+//  ESCore.h
+//  ESCore
+//
+//  Created by Eslam Hanafy on 12/29/19.
+//  Copyright © 2019 Eslam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ESCore.
+FOUNDATION_EXPORT double ESCoreVersionNumber;
+
+//! Project version string for ESCore.
+FOUNDATION_EXPORT const unsigned char ESCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ESCore/PublicHeader.h>
+
+
